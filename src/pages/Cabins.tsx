53c3cbeda43e8854a9fb5bdceab3e45fbcfd8010
samspi-tech <andrew.ryan.r.a@gmail.com>
2025-10-04 16:21:44 +0200

    connect to back-end with Supabase

--- src/pages/Cabins.tsx
+++ src/pages/Cabins.tsx
@@ -1,7 +1,13 @@
 import Row from '../ui/Row';
+import { useEffect } from 'react';
 import Heading from '../ui/Heading';
+import { getAllCabins } from '../lib/supabase/services/cabin.service';
 
 export default function Cabins() {
+    useEffect(() => {
+        getAllCabins().then((data) => console.log(data));
+    }, []);
+
     return (
         <Row type="horizontal">
             <Heading as="h1">All cabins</Heading>
