bb09400ce58c2fc50f6321336b9a37af4af06c19
samspi-tech <andrew.ryan.r.a@gmail.com>
2025-10-04 18:22:42 +0200

    setup react-query

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,26 @@ import Account from './pages/Account';
 import AppLayout from './ui/AppLayout';
 import Bookings from './pages/Bookings';
 import Settings from './pages/Settings';
+import { ONE_MINUTE } from './constants';
 import Dashboard from './pages/Dashboard';
 import PageNotFound from './pages/PageNotFound';
 import GlobalStyles from './styles/GlobalStyles';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: ONE_MINUTE,
+        },
+    },
+});
 
 export default function App() {
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false} />
+
             <GlobalStyles />
             <Router>
                 <Routes>
@@ -30,6 +43,6 @@ export default function App() {
                     <Route path="*" element={<PageNotFound />} />
                 </Routes>
             </Router>
-        </>
+        </QueryClientProvider>
     );
 }
