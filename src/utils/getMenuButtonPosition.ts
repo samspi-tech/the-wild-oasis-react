c928e99e283e2c2ab4ec18ba66c2b6d582272c08
samspi-tech <andrew.ryan.r.a@gmail.com>
2025-10-25 15:04:39 +0200

    refactor

--- src/utils/getMenuButtonPosition.ts
+++ src/utils/getMenuButtonPosition.ts
@@ -4,11 +4,13 @@ import { EIGHT_PIXELS } from './amounts';
 export function getMenuButtonPosition(e: MouseEvent) {
     if (e.target instanceof Element) {
         const button = e.target.closest('button');
-        const rect = button?.getBoundingClientRect();
+        const buttonRect = button?.getBoundingClientRect()!;
 
-        return {
-            x: window.innerWidth - rect!.width - rect!.x,
-            y: rect!.y + rect!.height + EIGHT_PIXELS,
+        const buttonPosition = {
+            y: buttonRect.y + buttonRect.height + EIGHT_PIXELS,
+            x: window.innerWidth - buttonRect.width - buttonRect.x,
         };
+
+        return buttonPosition;
     }
 }
