bce6f2665ddd495587925ef0cb09fbb782723cfc
samspi-tech <andrew.ryan.r.a@gmail.com>
2025-10-25 22:49:05 +0200

    refactor

--- src/utils/getPages.ts
+++ src/utils/getPages.ts
@@ -1,8 +1,8 @@
 import { PAGE_SIZE } from './amounts';
 
 export function getPages(searchParams: URLSearchParams) {
-    const pageValue = Number(searchParams.get('page'));
-    const currentPage = pageValue ?? 1;
+    const pageValue = searchParams.get('page');
+    const currentPage = Number(pageValue) ?? 1;
 
     const from = (currentPage - 1) * PAGE_SIZE;
     const to = from + PAGE_SIZE - 1;
